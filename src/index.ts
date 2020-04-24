06ac0345b17b8ee2b518f950e063265515fa5e7d
Neal Callaghan <neal.callaghan@blueprism.com>
2020-04-24 14:37:44 +0100

    added when to typeswitch, amended the index.ts file.

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,9 @@
-export type TypeConstructor = new (...args: any[]) => any
+import { Case, When, Default, TypeSwitch, TypeSwitchWithDefault,  } from './typeswitch';
 
-export interface HasConstructor {
-  constructor: { name: string }
-}
-
-export interface CaseResult {
-  Type: TypeConstructor,
-  Func: (arg: InstanceType<TypeConstructor>) => any
-}
-
-export interface DefaultResult {
-  Func: () => any
-}
-
-export type CaseFunc = <T extends TypeConstructor>(type: T) => (func: (arg: InstanceType<T>) => any) => CaseResult
-
-export const Case:CaseFunc = <TOut>(type: TypeConstructor) => (func: (arg: InstanceType<TypeConstructor>) => TOut) => {
-  const caseResult:CaseResult = {
-    Type: type,
-    Func: (a: InstanceType<TypeConstructor>) => func(a),
-  }
-  return caseResult;
-}
-
-export type DefaultFunc = (func: () => any) => DefaultResult
-
-export const Default: DefaultFunc = (func: () => any): DefaultResult => {
-  const result: DefaultResult = {
-    Func: func,
-  }
-  return result;
-}
-
-export const TypeSwitchWithDefault = (arg: any) => (...funcs: CaseResult[]) => (def?: DefaultResult) => {
-
-    const func = funcs.find(t => t.Type.name === arg.constructor.name) || def;
-    if (!func) {
-        throw Error('you must provide a case or default value');
-    }
-    
-    return func.Func(arg);      
-}
-
-export const TypeSwitch = (arg: any) => (...funcs: CaseResult[]) => TypeSwitchWithDefault(arg)(...funcs)();
+export default {
+  Case,
+  When,
+  Default,
+  TypeSwitch,
+  TypeSwitchWithDefault,
+};
--- src/typeswitch.ts
+++ src/typeswitch.ts
@@ -1,45 +1,54 @@
-export type TypeConstructor = new (...args: any[]) => any
+type TypeConstructor = new (...args: any[]) => any;
 
-export interface HasConstructor {
-  constructor: { name: string }
-}
+type WhenResult<T> = (arg: T) => boolean;
+export const When = <T>(func: (arg: T) => boolean):WhenResult<T> => (arg: T) => func(arg);
 
 export interface CaseResult {
   Type: TypeConstructor,
-  Func: (arg: InstanceType<TypeConstructor>) => any
+  Func: (arg: InstanceType<TypeConstructor>) => any,
+  When?: WhenResult<InstanceType<TypeConstructor>>,
 }
 
-export interface DefaultResult {
-  Func: () => any
-}
+type CaseFunc = <T extends TypeConstructor>(type: T, whenFunc?: WhenResult<InstanceType<T>>) => (func: (arg: InstanceType<T>) => any) => CaseResult;
 
-export type CaseFunc = <T extends TypeConstructor>(type: T) => (func: (arg: InstanceType<T>) => any) => CaseResult
-
-export const Case:CaseFunc = <TOut>(type: TypeConstructor) => (func: (arg: InstanceType<TypeConstructor>) => TOut) => {
+export const Case:CaseFunc = <TOut>(type: TypeConstructor, whenFunc?: WhenResult<InstanceType<TypeConstructor>>) => (func: (arg: InstanceType<TypeConstructor>) => TOut) => {
   const caseResult:CaseResult = {
     Type: type,
     Func: (a: InstanceType<TypeConstructor>) => func(a),
+    When: whenFunc,
   }
   return caseResult;
+};
+
+export interface DefaultResult {
+  Func: () => any,
 }
 
-export type DefaultFunc = (func: () => any) => DefaultResult
+type DefaultFunc = (func: () => any) => DefaultResult;
 
 export const Default: DefaultFunc = (func: () => any): DefaultResult => {
   const result: DefaultResult = {
     Func: func,
   }
   return result;
-}
+};
+
+const findMatchingCaseResult = (arg: any, caseResult: CaseResult): boolean => {
+  
+  const matchingConstructors = caseResult.Type.name === arg.constructor.name;
+  if (!caseResult.When) return matchingConstructors;
+
+  return matchingConstructors && caseResult.When(arg);
+};
 
 export const TypeSwitchWithDefault = (arg: any) => (...funcs: CaseResult[]) => (def?: DefaultResult) => {
 
-    const func = funcs.find(t => t.Type.name === arg.constructor.name) || def;
-    if (!func) {
-        throw Error('you must provide a case or default value');
-    }
-    
-    return func.Func(arg);      
-}
+  const func = funcs.find(t => findMatchingCaseResult(arg, t)) || def;
+  if (!func) {
+      throw Error('TypeSwitch error you must provide a case or default value');
+  }
+  
+  return func.Func(arg);      
+};
 
 export const TypeSwitch = (arg: any) => (...funcs: CaseResult[]) => TypeSwitchWithDefault(arg)(...funcs)();
